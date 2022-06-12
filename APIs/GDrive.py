2794e34b1af8c7e30de77d295b59a43143c0bca6
DanieleMarobin <daniele.marobin@hotmail.com>
2022-06-12 23:49:42 +0200

    commit

--- APIs/GDrive.py
+++ APIs/GDrive.py
@@ -5,11 +5,10 @@
 # from google.oauth2.credentials import Credentials
 # from googleapiclient.discovery import build
 
-
 # # If modifying these scopes, delete the file token.json.
 # SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
 
-# def print_files():
+# def get_gdrive_files_metadata():
 #     """Shows basic usage of the Drive v3 API.
 #     Prints the names and ids of the first 10 files the user has access to.
 #     """
@@ -21,11 +20,10 @@
 #         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
 #     # If there are no (valid) credentials available, let the user log in.
 
-
 #     service = build('drive', 'v3', credentials=creds)
 
 #     # Call the Drive v3 API
-#     results = service.files().list(pageSize=10, fields="nextPageToken, files(id, name)").execute()
+#     results = service.files().list(pageSize=100, fields="nextPageToken, files(id, name)").execute()
 #     items = results.get('files', [])
 
 #     if not items:
