97bc766526c7b4f574b5ac3df62b9068919ea31f
DanieleMarobin <daniele.marobin@hotmail.com>
2022-06-13 16:56:41 +0200

    commit

--- Home.py
+++ Home.py
@@ -4,15 +4,15 @@ import streamlit as st
 
 
 
-st.set_page_config(page_title="Home",layout="wide",initial_sidebar_state="expanded")
+# st.set_page_config(page_title="Home",layout="wide",initial_sidebar_state="expanded")
 
 
-if 'prediction' not in st.session_state:
-    st.session_state['prediction'] = 0
-if 'count' not in st.session_state:
-    st.session_state['count'] = 0
-if 'dates' not in st.session_state:
-    st.session_state['dates'] = {}
+# if 'prediction' not in st.session_state:
+#     st.session_state['prediction'] = 0
+# if 'count' not in st.session_state:
+#     st.session_state['count'] = 0
+# if 'dates' not in st.session_state:
+#     st.session_state['dates'] = {}
 
 
 st.sidebar.markdown("# Home")
