483d44d8b548d03ee5839e0c298c501a1ac663f0
DanieleMarobin <daniele.marobin@hotmail.com>
2022-06-28 10:56:04 +0200

    commit

--- pages/Corn Yield.py
+++ pages/Corn Yield.py
@@ -19,6 +19,9 @@ su.initialize_Monitor_Soybean_USA()
 st.set_page_config(page_title="Corn Yield",layout="wide",initial_sidebar_state="expanded")
 
 # Title, Declarations
+season_start = dt(GV.CUR_YEAR,4,10)
+season_end = dt(GV.CUR_YEAR,9,20)
+
 st.markdown("## Corn Yield")
 st.markdown("---")
 
@@ -111,7 +114,7 @@ if st.session_state[pf+'update']:
     # Trend Yield
     trend_DF_instr=um.Build_DF_Instructions('weighted', prec_units=prec_units, temp_units=temp_units)
 
-    pred_df['Trend'] = cy.Build_Progressive_Pred_DF(raw_data, milestones, trend_DF_instr,GV.CUR_YEAR, dt(2022,4,10), dt(2022,9,20),trend_yield_case=True)
+    pred_df['Trend'] = cy.Build_Progressive_Pred_DF(raw_data, milestones, trend_DF_instr,GV.CUR_YEAR, season_start, season_end,trend_yield_case=True)
     yields['Trend'] = model.predict(pred_df['Trend'][model.params.index]).values
     pred_df['Trend']['Yield']=yields['Trend']       
     prog=0.7
@@ -132,7 +135,7 @@ if st.session_state[pf+'update']:
         # pred_df[WD] = cy.Build_Pred_DF(raw_data, milestones, pred_DF_instr,GV.CUR_YEAR, yield_analysis_start)
         # pred_df[WD] = cy.Build_Pred_DF(raw_data, milestones, pred_DF_instr,GV.CUR_YEAR, yield_analysis_start, date_end=dt(2022,9,30))
 
-        pred_df[WD] = cy.Build_Progressive_Pred_DF(raw_data, milestones, pred_DF_instr,GV.CUR_YEAR, dt(2022,4,10), dt(2022,9,20))
+        pred_df[WD] = cy.Build_Progressive_Pred_DF(raw_data, milestones, pred_DF_instr,GV.CUR_YEAR, season_start, season_end)
 
         yields[WD] = model.predict(pred_df[WD][model.params.index]).values        
         pred_df[WD]['Yield']=yields[WD]
@@ -169,171 +172,187 @@ for i,WD in enumerate(sel_WD):
 # metric_empty.metric(label='Yield', value="{:.2f}".format(yields[-1]), delta= "{:.2f}".format(yields[-1]-yields[-2])+" bu/Ac")
 
 # _______________________________________ CHART _______________________________________
-last_HIST_day = raw_data['w_df_all'][GV.WD_HIST].last_valid_index()
-last_GFS_day = raw_data['w_df_all'][GV.WD_GFS].last_valid_index()
-last_ECMWF_day = raw_data['w_df_all'][GV.WD_ECMWF].last_valid_index()
-last_day = pred_df[sel_WD[0]].last_valid_index()
+if True:
+    last_HIST_day = raw_data['w_df_all'][GV.WD_HIST].last_valid_index()
+    last_GFS_day = raw_data['w_df_all'][GV.WD_GFS].last_valid_index()
+    last_ECMWF_day = raw_data['w_df_all'][GV.WD_ECMWF].last_valid_index()
+    last_day = pred_df[sel_WD[0]].last_valid_index()
 
-# Trend Yield
-df = pred_df['Trend']
-final_yield = yields['Trend'][-1]
-label_trend = "Trend: "+"{:.2f}".format(final_yield)
-yield_chart=uc.line_chart(x=pd.to_datetime(df.index.values), y=df['Yield'],name=label_trend,color='black', mode='lines',height=750)
-font=dict(size=20,color="black")
-yield_chart.add_annotation(x=last_day, y=final_yield,text=label_trend,showarrow=False,arrowhead=1,font=font,yshift=+20)
+    # Trend Yield
+    df = pred_df['Trend']
+    final_yield = yields['Trend'][-1]
+    label_trend = "Trend: "+"{:.2f}".format(final_yield)
+    yield_chart=uc.line_chart(x=pd.to_datetime(df.index.values), y=df['Yield'],name=label_trend,color='black', mode='lines',height=750)
+    font=dict(size=20,color="black")
+    yield_chart.add_annotation(x=last_day, y=final_yield,text=label_trend,showarrow=False,arrowhead=1,font=font,yshift=+20)
 
-# Historical Weather
-df = pred_df[GV.WD_HIST]
-df=df[df.index<=last_HIST_day]
-uc.add_series(yield_chart, x=pd.to_datetime(df.index.values), y=df['Yield'], mode='lines+markers', name='Realized Weather', color='black', showlegend=True, legendrank=3)
+    # Historical Weather
+    df = pred_df[GV.WD_HIST]
+    df=df[df.index<=last_HIST_day]
+    uc.add_series(yield_chart, x=pd.to_datetime(df.index.values), y=df['Yield'], mode='lines+markers', name='Realized Weather', color='black', showlegend=True, legendrank=3)
 
 
-# Forecasts Historical
-df = pred_df[GV.WD_HIST]
-df=df[df.index>last_HIST_day]
-final_yield = yields[GV.WD_HIST][-1]
-label_hist = "Hist: "+"{:.2f}".format(final_yield)
-uc.add_series(yield_chart, x=pd.to_datetime(df.index.values), y=df['Yield'], mode='lines+markers', name=label_hist, color='orange', marker_size=5, line_width=1.0, showlegend=True, legendrank=1)
+    # Forecasts Historical
+    df = pred_df[GV.WD_HIST]
+    df=df[df.index>last_HIST_day]
+    final_yield = yields[GV.WD_HIST][-1]
+    label_hist = "Hist: "+"{:.2f}".format(final_yield)
+    uc.add_series(yield_chart, x=pd.to_datetime(df.index.values), y=df['Yield'], mode='lines+markers', name=label_hist, color='orange', marker_size=5, line_width=1.0, showlegend=True, legendrank=1)
 
-# Forecasts GFS
-df = pred_df[GV.WD_H_GFS]
-df=df[df.index>last_HIST_day]
-final_yield = yields[GV.WD_H_GFS][-1]
-label_gfs = "GFS: "+"{:.2f}".format(final_yield)
-uc.add_series(yield_chart, x=pd.to_datetime(df.index.values), y=df['Yield'], mode='lines+markers', name=label_gfs, color='blue', marker_size=5, line_width=1.0, showlegend=True, legendrank=1)
+    # Forecasts GFS
+    df = pred_df[GV.WD_H_GFS]
+    df=df[df.index>last_HIST_day]
+    final_yield = yields[GV.WD_H_GFS][-1]
+    label_gfs = "GFS: "+"{:.2f}".format(final_yield)
+    uc.add_series(yield_chart, x=pd.to_datetime(df.index.values), y=df['Yield'], mode='lines+markers', name=label_gfs, color='blue', marker_size=5, line_width=1.0, showlegend=True, legendrank=1)
 
-# Forecasts ECMWF
-df = pred_df[GV.WD_H_ECMWF]
-df=df[df.index>last_HIST_day]
-final_yield = yields[GV.WD_H_ECMWF][-1]
-label_ecmwf = "ECMWF: "+"{:.2f}".format(final_yield)
-uc.add_series(yield_chart, x=pd.to_datetime(df.index.values), y=df['Yield'], mode='lines+markers', name=label_ecmwf, color='green', marker_size=5, line_width=1.0, showlegend=True, legendrank=2)
+    # Forecasts ECMWF
+    df = pred_df[GV.WD_H_ECMWF]
+    df=df[df.index>last_HIST_day]
+    final_yield = yields[GV.WD_H_ECMWF][-1]
+    label_ecmwf = "ECMWF: "+"{:.2f}".format(final_yield)
+    uc.add_series(yield_chart, x=pd.to_datetime(df.index.values), y=df['Yield'], mode='lines+markers', name=label_ecmwf, color='green', marker_size=5, line_width=1.0, showlegend=True, legendrank=2)
 
-# Projection Historical
-df = pred_df[GV.WD_HIST]
-df=df[df.index>last_HIST_day]
-uc.add_series(yield_chart, x=pd.to_datetime(df.index.values), y=df['Yield'], mode='lines', name='Projections', color='red', line_width=2.0, showlegend=True, legendrank=4)
-final_yield = yields[GV.WD_HIST][-1]
-font=dict(size=20,color="red")
-yield_chart.add_annotation(x=last_day, y=final_yield,text=label_hist,showarrow=False,arrowhead=1,font=font,yshift=+15)
+    # Projection Historical
+    df = pred_df[GV.WD_HIST]
+    df=df[df.index>last_HIST_day]
+    uc.add_series(yield_chart, x=pd.to_datetime(df.index.values), y=df['Yield'], mode='lines', name='Projections', color='red', line_width=2.0, showlegend=True, legendrank=4)
+    final_yield = yields[GV.WD_HIST][-1]
+    font=dict(size=20,color="red")
+    yield_chart.add_annotation(x=last_day, y=final_yield,text=label_hist,showarrow=False,arrowhead=1,font=font,yshift=+15)
 
-# If GFS has higher yield, shift its label up and ecmwf down
-if (yields[GV.WD_H_GFS][-1] > yields[GV.WD_H_ECMWF][-1]):
-    gfs_y_shift=15
-    ecmwf_y_shift=-15
-else:
-    gfs_y_shift=-15
-    ecmwf_y_shift=15
+    # If GFS has higher yield, shift its label up and ecmwf down
+    if (yields[GV.WD_H_GFS][-1] > yields[GV.WD_H_ECMWF][-1]):
+        gfs_y_shift=10
+        ecmwf_y_shift=-20
+    else:
+        gfs_y_shift=-20
+        ecmwf_y_shift=10
 
-# Projection GFS
-df = pred_df[GV.WD_H_GFS]
-df=df[df.index>last_GFS_day]
-uc.add_series(yield_chart, x=pd.to_datetime(df.index.values), y=df['Yield'], mode='lines', name='Projections', color='red', line_width=2.0, showlegend=False, legendrank=4)
-final_yield = yields[GV.WD_H_GFS][-1]
-font=dict(size=20,color="blue")
-yield_chart.add_annotation(x=last_day, y=final_yield,text=label_gfs,showarrow=False,arrowhead=1,font=font,yshift=gfs_y_shift)
+    # Projection GFS
+    df = pred_df[GV.WD_H_GFS]
+    df=df[df.index>last_GFS_day]
+    uc.add_series(yield_chart, x=pd.to_datetime(df.index.values), y=df['Yield'], mode='lines', name='Projections', color='red', line_width=2.0, showlegend=False, legendrank=4)
+    final_yield = yields[GV.WD_H_GFS][-1]
+    font=dict(size=20,color="blue")
+    yield_chart.add_annotation(x=last_day, y=final_yield,text=label_gfs,showarrow=False,arrowhead=1,font=font,yshift=gfs_y_shift)
 
-# Projection ECMWF
-df = pred_df[GV.WD_H_ECMWF]
-df=df[df.index>last_ECMWF_day]
-uc.add_series(yield_chart, x=pd.to_datetime(df.index.values), y=df['Yield'], mode='lines', name='Projection', color='red', line_width=2.0, showlegend=False, legendrank=4)
-final_yield = yields[GV.WD_H_ECMWF][-1]
-font=dict(size=20,color="green")
-yield_chart.add_annotation(x=last_day, y=final_yield,text=label_ecmwf,showarrow=False,arrowhead=1,font=font,yshift=ecmwf_y_shift)
+    # Projection ECMWF
+    df = pred_df[GV.WD_H_ECMWF]
+    df=df[df.index>last_ECMWF_day]
+    uc.add_series(yield_chart, x=pd.to_datetime(df.index.values), y=df['Yield'], mode='lines', name='Projection', color='red', line_width=2.0, showlegend=False, legendrank=4)
+    final_yield = yields[GV.WD_H_ECMWF][-1]
+    font=dict(size=20,color="green")
+    yield_chart.add_annotation(x=last_day, y=final_yield,text=label_ecmwf,showarrow=False,arrowhead=1,font=font,yshift=ecmwf_y_shift)
 
-add_intervals(yield_chart, intervals)
+    add_intervals(yield_chart, intervals)
 
-st.plotly_chart(yield_chart)
+    st.plotly_chart(yield_chart)
 
-st.markdown('---')
+    st.markdown('---')
 
 # -------------------------------------------- Model Details --------------------------------------------
-# Coefficients
-st_model_coeff=pd.DataFrame(columns=model.params.index)
-st_model_coeff.loc[len(st_model_coeff)]=model.params.values
-st_model_coeff.index=['Model Coefficients']
+# Yield impact decomposition
+if False:
+    st.dataframe(pred_df['Trend'].loc[season_end:season_end])
+    input_arr = pred_df['Trend'].loc[season_end:season_end][model.params.index].values
+    coeff_arr = model.params.values
+    st.write('Input', input_arr)
+    st.write('Coeff', coeff_arr)
+    st.write('Yield Contribution', input_arr*coeff_arr)
 
-st.markdown('##### Coefficients')
-st.dataframe(st_model_coeff)
-st.markdown('---')
+
+# Coefficients
+if True:
+    st_model_coeff=pd.DataFrame(columns=model.params.index)
+    st_model_coeff.loc[len(st_model_coeff)]=model.params.values
+    st_model_coeff.index=['Model Coefficients']
+
+    st.markdown('##### Coefficients')
+    st.dataframe(st_model_coeff)
+    st.markdown('---')
 
 # Prediction DataSets
-st.markdown('##### Trend DataSet')
-st.dataframe(pred_df['Trend'].drop(columns=['const']))
+if True:
+    st.markdown('##### Trend DataSet')
+    st.dataframe(pred_df['Trend'].drop(columns=['const']))
 
-for WD in sel_WD:
-    st.markdown('##### Prediction DataSet - ' + s_WD[WD])
-    st.dataframe(st.session_state[pf+'pred_df'][WD].drop(columns=['const']))
+    for WD in sel_WD:
+        st.markdown('##### Prediction DataSet - ' + s_WD[WD])
+        st.dataframe(st.session_state[pf+'pred_df'][WD].drop(columns=['const']))
 
 
 # Training DataSet
-st_train_df = deepcopy(train_df)
-st.markdown('##### Training DataSet')
-st.dataframe(st_train_df.sort_index(ascending=False).loc[st_train_df['Trend']<GV.CUR_YEAR])
-st.markdown("---")
+if True:
+    st_train_df = deepcopy(train_df)
+    st.markdown('##### Training DataSet')
+    st.dataframe(st_train_df.sort_index(ascending=False).loc[st_train_df['Trend']<GV.CUR_YEAR])
+    st.markdown("---")
 
 # -------------------------------------------- Milestones --------------------------------------------
-dates_fmt = "%d %b %Y"
-milestones_col, _ , intervals_col = st.columns([2,   0.5,   6])
-with milestones_col:
-    st.markdown('##### Milestones')
+if True:
+    dates_fmt = "%d %b %Y"
+    milestones_col, _ , intervals_col = st.columns([2,   0.5,   6])
+    with milestones_col:
+        st.markdown('##### Milestones')
 
-with intervals_col:
-    st.markdown('##### Weather Windows')
+    with intervals_col:
+        st.markdown('##### Weather Windows')
 
-col_80_planted, col_50_silked ,_, i_1, i_2, i_3, i_4 = st.columns([1,1,   0.5,   1.5,1.5,1.5,1.5])
+    col_80_planted, col_50_silked ,_, i_1, i_2, i_3, i_4 = st.columns([1,1,   0.5,   1.5,1.5,1.5,1.5])
 
-# 80% Planted
-with col_80_planted:
-    st.markdown('##### 80% Planted')  
-    st.write('Self-explanatory')  
-    styler = st.session_state[pf+'milestones']['80_pct_planted'].sort_index(ascending=False).style.format({"date": lambda t: t.strftime(dates_fmt)})
-    st.write(styler)
+    # 80% Planted
+    with col_80_planted:
+        st.markdown('##### 80% Planted')  
+        st.write('Self-explanatory')  
+        styler = st.session_state[pf+'milestones']['80_pct_planted'].sort_index(ascending=False).style.format({"date": lambda t: t.strftime(dates_fmt)})
+        st.write(styler)
 
-# 50% Silking
-with col_50_silked:
-    st.markdown('##### 50% Silking')
-    st.write('Self-explanatory')  
-    styler = st.session_state[pf+'milestones']['50_pct_silked'].sort_index(ascending=False).style.format({"date": lambda t: t.strftime(dates_fmt)})
-    st.write(styler)    
+    # 50% Silking
+    with col_50_silked:
+        st.markdown('##### 50% Silking')
+        st.write('Self-explanatory')  
+        styler = st.session_state[pf+'milestones']['50_pct_silked'].sort_index(ascending=False).style.format({"date": lambda t: t.strftime(dates_fmt)})
+        st.write(styler)    
 
 # -------------------------------------------- Intervals --------------------------------------------
-# Planting_Prec
-with i_1:
-    st.markdown('##### Planting Prec')
-    st.write('80% planted -40 and +25 days')
-    styler = st.session_state[pf+'intervals']['planting_interval'].sort_index(ascending=False).style.format({"start": lambda t: t.strftime(dates_fmt),"end": lambda t: t.strftime(dates_fmt)})
-    st.write(styler)
+if True:
+    # Planting_Prec
+    with i_1:
+        st.markdown('##### Planting Prec')
+        st.write('80% planted -40 and +25 days')
+        styler = st.session_state[pf+'intervals']['planting_interval'].sort_index(ascending=False).style.format({"start": lambda t: t.strftime(dates_fmt),"end": lambda t: t.strftime(dates_fmt)})
+        st.write(styler)
 
-# Jul_Aug_Prec
-with i_2:
-    st.markdown('##### Jul-Aug Prec')
-    st.write('80% planted +26 and +105 days')
-    styler = st.session_state[pf+'intervals']['jul_aug_interval'].sort_index(ascending=False).style.format({"start": lambda t: t.strftime(dates_fmt),"end": lambda t: t.strftime(dates_fmt)})
-    st.write(styler)
+    # Jul_Aug_Prec
+    with i_2:
+        st.markdown('##### Jul-Aug Prec')
+        st.write('80% planted +26 and +105 days')
+        styler = st.session_state[pf+'intervals']['jul_aug_interval'].sort_index(ascending=False).style.format({"start": lambda t: t.strftime(dates_fmt),"end": lambda t: t.strftime(dates_fmt)})
+        st.write(styler)
 
-# Pollination_SDD
-with i_3:
-    # 50% Silking -15 and +15 days
-    st.markdown('##### Pollination SDD')
-    st.write('50% Silking -15 and +15 days')
-    styler = st.session_state[pf+'intervals']['pollination_interval'].sort_index(ascending=False).style.format({"start": lambda t: t.strftime(dates_fmt),"end": lambda t: t.strftime(dates_fmt)})
-    st.write(styler)
+    # Pollination_SDD
+    with i_3:
+        # 50% Silking -15 and +15 days
+        st.markdown('##### Pollination SDD')
+        st.write('50% Silking -15 and +15 days')
+        styler = st.session_state[pf+'intervals']['pollination_interval'].sort_index(ascending=False).style.format({"start": lambda t: t.strftime(dates_fmt),"end": lambda t: t.strftime(dates_fmt)})
+        st.write(styler)
 
-# Regular_SDD
-with i_4:
-    st.markdown('##### Regular SDD')
-    st.write('20 Jun - 15 Sep')
-    styler = st.session_state[pf+'intervals']['regular_interval'].sort_index(ascending=False).style.format({"start": lambda t: t.strftime(dates_fmt),"end": lambda t: t.strftime(dates_fmt)})
-    st.write(styler)
-    
-st.markdown("---")
+    # Regular_SDD
+    with i_4:
+        st.markdown('##### Regular SDD')
+        st.write('20 Jun - 15 Sep')
+        styler = st.session_state[pf+'intervals']['regular_interval'].sort_index(ascending=False).style.format({"start": lambda t: t.strftime(dates_fmt),"end": lambda t: t.strftime(dates_fmt)})
+        st.write(styler)
+        
+    st.markdown("---")
 
-# -------------------------------------------- Summary --------------------------------------------
-# Summary
-st.subheader('Model Summary:')
-st.write(model.summary())
-st.markdown("---")
+# Stat Model Summary
+if True:
+    st.subheader('Model Summary:')
+    st.write(model.summary())
+    st.markdown("---")
 
 # Analog Scenarios results
 if False:
@@ -349,8 +368,9 @@ if False:
     st.markdown("---")
 
 # Correlation Matrix
-st.subheader('Correlation Matrix:')
-st.plotly_chart(um.chart_corr_matrix(train_df.drop(columns=['Yield','const'])))
+if True:
+    st.subheader('Correlation Matrix:')
+    st.plotly_chart(um.chart_corr_matrix(train_df.drop(columns=['Yield','const'])))
 
 
 
